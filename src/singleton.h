7a97f252c31bb02a9d07c683a913880ea23da94f
Netzmb <netzmb@gmail.com>
2012-12-10 17:15:14 +0200

    delete useless help example

--- src/singleton.h
+++ src/singleton.h
@@ -19,73 +19,11 @@ class Singleton {
         return *_instance;
     }
 
-
-
-
 //    protected:
 //    Singleton();
 
     private:
 };
-
-/*
- * examples
- *
-
-class Graphics : public Singleton<Graphics> {
-    public:
-
-    enum Side {
-        SIDE_NONE,
-        SIDE_TOP,
-        SIDE_BOTTOM
-        };
-
-    bool init() { _side = SIDE_NONE; return true;};
-
-    void setSide(Side side) { _side = side; } ;
-    void putSide() const  {std::cout << (int)_side << std::endl;} ;
-
-
-    private:
-
-    int _side;
-
-
-};
-
-
-class Audio : public Singleton<Audio> {
-    public:
-    void putSide() const  {std::cout << "audio" << std::endl;} ;
-
-};
-
-
-
-int main() {
-
-//    Graphics::instance().testSide(GraphicsClass::SIDE_TOP);
-    Graphics::instance();
-    Graphics::instance();
-    Graphics::instance().init();
-    Graphics::instance().putSide();
-    Graphics::instance().setSide(Graphics::SIDE_BOTTOM);
-    Graphics::instance().putSide();
-    Graphics::instance().setSide(Graphics::SIDE_TOP);
-    Graphics::instance().putSide();
-    Graphics::instance();
-
-    Audio::instance().putSide();
-    Graphics::instance().putSide();
-    Audio::instance().putSide();
-    Graphics::instance().putSide();
-
-    return 0;
-}
-
-*/
-
 
 #endif /* _SINGLETON_H_ */
 
